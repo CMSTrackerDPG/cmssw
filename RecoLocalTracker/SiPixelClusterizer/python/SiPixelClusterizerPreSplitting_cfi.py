d9f753d72ef64dcb8ed126ff13fbe93dc45b714b
Tatjana <tatjana.susa@cern.ch>
2020-10-09 02:07:23 +0200

    Introduce siPixelDigiMorphing ProcessModifier

--- RecoLocalTracker/SiPixelClusterizer/python/SiPixelClusterizerPreSplitting_cfi.py
+++ RecoLocalTracker/SiPixelClusterizer/python/SiPixelClusterizerPreSplitting_cfi.py
@@ -5,3 +5,9 @@ import FWCore.ParameterSet.Config as cms
 from CondTools.SiPixel.SiPixelGainCalibrationService_cfi import *
 from RecoLocalTracker.SiPixelClusterizer.SiPixelClusterizer_cfi import siPixelClusters as _siPixelClusters
 siPixelClustersPreSplitting = _siPixelClusters.clone()
+
+from Configuration.ProcessModifiers.siPixelDigiMorphing_cff import *
+siPixelDigiMorphing.toModify(
+    siPixelClustersPreSplitting,
+    src = cms.InputTag('siPixelDigisMorphed')
+    )
